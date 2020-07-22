422aca33ad5bd059a89ddbf9088f20f924ffc54f
Samara Turganbaeva <samaraturganbaeva@Samaras-MacBook-Pro.local>
2020-07-22 16:08:40 -0500

    initial commit

---
+++ src/test/java/step_defs/HelloWorld.java
@@ -0,0 +1,10 @@
+package step_defs;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("I love life");
+        System.out.println("This world is beautiful");
+    }
+}
