360df2ac12b6220a9a7325c252ae772a8df11744
SamTSam <samaraturganbaeva@Samaras-MacBook-Pro.local>
2020-08-04 10:59:19 -0500

    CSVReader

---
+++ src/test/java/io_utils/CSVReader.java
@@ -0,0 +1,30 @@
+package io_utils;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+
+public class CSVReader {
+    private final String path;
+    private final FileReader reader; // reads byte by byte
+    private final BufferedReader bufferedReader;//reads line by line
+
+    public CSVReader(String path) throws FileNotFoundException {
+        this.path = path;
+        this.reader = new FileReader(path);
+        this.bufferedReader = new BufferedReader(reader);
+
+    }
+
+    public String read() throws IOException {
+        String line = bufferedReader.readLine();
+        return line;
+    }
+    public void close() throws  IOException {
+        String line = bufferedReader.readLine();
+
+
+    }
+}
